a4694b007db8e977520482d8f5d5bc199d94e20b
Pierre-Henry Soria <hi@ph7.me>
2021-10-23 21:33:32 +1030

    Add Monolog (first initial step)

--- bin/generate.php
+++ bin/generate.php
@@ -8,7 +8,17 @@ define('APP_VERSION', '1.0.0')
 
 require ROOT_DIR . '/vendor/autoload.php';
 
+use Monolog\{
+    Logger, Handler\StreamHandler
+};
+use PH7\PhpReadmeGeneratorFile\Command\Markdown\{
+    Generator,
+    Information
+};
 use Symfony\Component\Console\Application;
+
+$logger = new Logger('ReadmeGenerator');
+$logger->pushHandler(new StreamHandler("php://stdout", Logger::DEBUG));
 
 $app = new Application(
     APP_NAME,
