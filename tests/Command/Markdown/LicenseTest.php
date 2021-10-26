fbe583edbe330427b9f3da7373eff3970b59836b
Pierre-Henry Soria <hi@ph7.me>
2021-10-26 19:46:07 +1030

    Add tests for License class's logic

---
+++ tests/Command/Markdown/LicenseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PH7\PhpReadmeGeneratorFile\Tests\Command\Markdown;
+
+use PH7\PhpReadmeGeneratorFile\Command\Markdown\License;
+use PHPUnit\Framework\TestCase;
+
+class LicenseTest extends TestCase
+{
+    /**
+     * @dataProvider licenseCodeProvider
+     */
+    public function testLinkWithValidLicenseCode(string $licenseCode): void
+    {
+        $actual = License::getLicenseLink($licenseCode);
+        $this->assertsame("https://opensource.org/licenses/{$licenseCode}", $actual);
+    }
+
+    public function testLinkWithInvalidLicenseCode(): void
+    {
+        $actual = License::getLicenseLink('INVALID');
+        $this->assertsame('https://opensource.org/licenses', $actual);
+    }
+
+    public function licenseCodeProvider(): array
+    {
+        return [License::CODES];
+    }
+
+}
