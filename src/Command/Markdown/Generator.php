0158bec6822e195f6108efcc8658efbfd6ed5e4b
Pierre-Henry Soria <hi@ph7.me>
2021-10-27 09:04:01 +1030

    Added `void` return type

--- src/Command/Markdown/Generator.php
+++ src/Command/Markdown/Generator.php
@@ -28,7 +28,7 @@ class Generator extends Command
         parent::__construct();
     }
 
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName('markdown:generate');
     }
