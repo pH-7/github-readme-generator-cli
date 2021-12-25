466c17ad9ad2a0453c8f35249175b84fa21bfd30
Pierre-Henry Soria <hi@ph7.me>
2021-12-25 15:29:07 +1030

    [save-code] Update script

--- save-code.sh
+++ save-code.sh
@@ -2,15 +2,17 @@
 
 # Save a git project to a specific repo (e.g. github, bitbucket, ...)
 function save-project-to-repo() {
+    branch="master"
+
     git remote rm origin
     git remote add origin $1
-    git push
+    git push origin $branch
 }
 
 declare readonly gitRemotes=(
     git@gitlab.com:pH-7/github-readme-generator-cli.git
-    git@bitbucket.org:pH_7/random-passcode-password.git
-    git@github.com:pH-7/php-github-readme-generator-seo-friendly.git
+    git@bitbucket.org:pH_7/php-github-readme-generator-seo-friendly.git
+    git@github.com:pH-7/github-readme-generator-cli.git
 )
 for remote in "${gitRemotes[@]}"
 do
